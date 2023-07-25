5360ef3bfe1996bdf3ef2f1f44eb0ee0e4b068ea
Naoto Kimura <nkimura2021infotech@gmail.com>
2023-07-25 22:48:02 +0900

    [modify] use recursive programming.

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,88 @@
-// code 1.2
 import p5 from "p5";
 
 const sketch = (p: p5) => {
   let a = 10
-  let b = 7
-  let scalar = 50
-  a *= scalar
-  b *= scalar
-
-  let wd = b
-  let xPos = 0
-  let yPos = 0
-  let itr = 0
-
-  let col
-  p.colorMode(p.HSB, 1)
+  let b = 13
+  let ratio = b / a
+  let thr = 40
 
   p.setup = () => {
-    p.createCanvas(p.windowWidth, p.windowHeight)
+    p.createCanvas(700, 700)
+    p.colorMode(p.HSB, 1)
+    divSquare(0, 0, p.width)
   }
   
   p.draw = () => {
-    while (wd > 0) {
+    // divSquare(0, 0, p.width)
+  }
+
+  function divSquare(xPos: number, yPos: number, wd: number): void {
+    let itr = 0
+    let xEndPos = wd + xPos
+    let yEndPos = wd + yPos
+    
+    p.fill(p.color(p.random(1), 1, 1))
+    p.rect(xPos, yPos, wd ,wd)
+
+    while (wd > thr) {
       itr++
       if (itr % 2 === 1) {
-        while (xPos + wd <= a) {
-          col = p.color(p.random(1), 1, 1)
-          p.fill(col)
-          p.rect(xPos, yPos, wd, wd)
-          xPos += wd
+        while (xPos + wd * ratio < xEndPos + 0.1) {
+          divRect(xPos, yPos, wd * ratio)
+          xPos += wd * ratio
         }
-        wd = a - xPos
+        wd = xEndPos - xPos
       } else {
-        while (yPos + wd <= b) {
-          col = p.color(p.random(1), 1, 1)
-          p.fill(col)
-          p.rect(xPos, yPos, wd, wd)
-          yPos += wd
+        while (yPos + wd / ratio < yEndPos + 0.1) {
+          divRect(xPos, yPos, wd)
+          yPos += wd / ratio
         }
-        wd = b - yPos
+        wd = yEndPos - yPos
       }
     }
+  }
+
+  function divRect(xPos: number, yPos: number, wd: number) {
+    let itr = 0
+    let xEndPos = xPos + wd
+    let yEndPos = yPos + wd / ratio
+
+    p.fill(p.color(p.random(1), 1, 1))
+    p.rect(xPos, yPos, wd, wd / ratio)
+
+    while (wd > thr) {
+      itr++
+      if (itr % 2 === 0) {
+        while (xPos + wd < xEndPos + 0.1) {
+          divSquare(xPos, yPos, wd)
+          xPos += wd
+        }
+        wd = xEndPos - xPos
+      } else {
+        while (yPos + wd < yEndPos + 0.1) {
+          divSquare(xPos, yPos, wd)
+          yPos += wd
+        }
+        wd = yEndPos - yPos
+      }
+    }
+  }
+
+  p.mouseClicked = () => {
+    let a = p.random(1, 20)
+    let b = p.random(1, 20)
+
+    while (a == b) {
+      b = p.random(1, 20)
+    }
+
+    thr = p.random(10, 300)
+
+    console.log("numA =", a, "numB =", b,"thr =", thr)
+
+    ratio = a / b
+    p.background(0, 0, 1)
+    divSquare(0, 0, p.width)
   }
 }
 
