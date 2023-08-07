8fb9a01a0bf5e766e68ed1b3d0cfa22e72ea58fd
Naoto Kimura <nkimura2021infotech@gmail.com>
2023-08-07 22:43:58 +0900

    20230807

--- src/main.js
+++ src/main.js
@@ -1,60 +1,6 @@
-let _angnoise, _radiusnoise
-let _xnoise, _ynoise
-let _angle
-let _radius
-let _strokeCol = 254
-let _strokeChange = -1
-
 function setup() {
-//   createCanvas(500, 500)
-  createCanvas(windowWidth, windowHeight)
-  smooth()
-  frameRate(30)
-  background(255)
-  noFill()
   
-  _angle = - PI / 2
-  
-  _angnoise = random(10)
-  _radiusnoise = random(10)
-  _xnoise = random(10)
-  _ynoise = random(10)
 }
 
 function draw() {
-  _radiusnoise += 0.005
-  _radius = (noise(_radiusnoise) * 550) + 1
-  
-  _angnoise += 0.005
-  _angle += (noise(_angnoise) * 6) - 3
-  if (_angle > 360) {
-    _angle -= 360
-  }
-  if (_angle < 0) {
-    _angle += 360
-  }
-  
-  _xnoise += 0.01
-  _ynoise += 0.01
-  let centerX = width / 2 + (noise(_xnoise) * 100) - 50
-  let centerY = height / 2 + (noise(_ynoise) * 100) - 50
-  
-  let rad = radians(_angle)
-  let x1 = centerX + (_radius * cos(rad))
-  let y1 = centerY + (_radius * sin(rad))
-  
-  let opprad = rad + PI
-  let x2 = centerX + (_radius * cos(opprad))
-  let y2 = centerY + (_radius * sin(opprad))
-  
-  _strokeCol += _strokeChange
-  if (_strokeCol > 254) {
-    _strokeChange = -1
-  }
-  if (_strokeCol < 0) {
-    _strokeChange = 1
-  }
-  stroke(_strokeCol, 60)
-  strokeWeight(1)
-  line(x1, y1, x2, y2)
 }
--- src/main.ts
+++ src/main.ts
@@ -1,112 +1,27 @@
 import p5 from "p5"
 
 const sketch = (p: p5) => {
-  let ratio = (1 + p.sqrt(5)) / 2
-  let thr = 80
-  let thr2 = 0.5
 
   p.setup = () => {
-    p.createCanvas(500, 500);
-    p.colorMode(p.HSB, 1);
-    colorRect(0, 0, p.width, p.width)
-    divSquare(0, 0, p.width)
-  }
+    p.createCanvas(800, 800)
+    p.smooth()
+    p.background(255)
 
-  function colorRect(xPos: number, yPos: number, wd: number, ht: number) {
-    let col
-    let val = p.random(1)
-    
-    if (val < 0.15) {
-      col = p.color(0, 1, 1)
-    } else if (val < 0.3) {
-      col = p.color(2.0 / 3, 1, 1)
-    } else if (val < 0.45) {
-      col = p.color(1.0 / 6, 1, 1)
-    } else if (val < 0.5) {
-      col = p.color(0, 1, 0)
-    } else if (val < 0.7) {
-      col = p.color(0, 0, 0.9)
-    } else {
-      col = p.color(0, 0, 1)
-    }
+    let xStart = p.random(10)
+    let xNoise = xStart
+    let yNoise = p.random(10)
 
-    p.fill(col)
-    p.strokeWeight(5)
-    p.rect(xPos, yPos, wd, ht)
-  }
-
-  function divRect(xPos: number, yPos: number, wd: number): void {
-    let itr = 0
-    let xEndPos = wd + xPos
-    let yEndPos = wd / ratio + yPos
-    
-    // p.fill(p.color(p.random(1), 1, 1))
-    // p.rect(xPos, yPos, wd ,wd)
-
-    while (wd > thr) {
-      itr++
-      if (itr % 2 === 0) {
-        while (xPos + wd < xEndPos + 0.1) {
-          colorRect(xPos, yPos, wd, wd)
-          if (p.random(1) < thr2) {
-            divSquare(xPos, yPos, wd)
-          }
-          xPos += wd
-        }
-        wd = xEndPos - xPos
-      } else {
-        while (yPos + wd < yEndPos + 0.1) {
-          colorRect(xPos, yPos, wd, wd)
-          if (p.random(1) < thr2) {
-            divSquare(xPos, yPos, wd)
-          }
-          yPos += wd
-        }
-        wd = yEndPos - yPos
+    for (let y = 0; y <= p.height; y += 1) {
+      yNoise += 0.01
+      xNoise = xStart
+      for (let x = 0; x <= p.width; x += 1) {
+        xNoise += 0.01
+        let alph =p.noise(xNoise, yNoise) * 255
+        p.stroke(0, alph)
+        p.line(x, y, x + 1, y + 1)
       }
     }
   }
-
-  function divSquare(xPos: number, yPos: number, wd: number) {
-    let itr = 0
-    let xEndPos = xPos + wd
-    let yEndPos = yPos + wd
-
-    // p.fill(p.color(p.random(1), 1, 1))
-    // p.rect(xPos, yPos, wd, wd / ratio)
-
-    while (wd > thr) {
-      itr++
-      if (itr % 2 === 1) {
-        while (xPos + wd * ratio < xEndPos + 0.1) {
-          colorRect(xPos, yPos, wd * ratio, wd)
-          if (p.random(1) < thr2) {
-            divRect(xPos, yPos, wd * ratio)
-          }
-          xPos += wd * ratio
-        }
-        wd = xEndPos - xPos
-      } else {
-        while (yPos + wd / ratio < yEndPos + 0.1) {
-          colorRect(xPos, yPos, wd, wd / ratio)
-          if (p.random(1) < thr2) {
-            divRect(xPos, yPos, wd)
-          }
-          yPos += wd / ratio
-        }
-        wd = yEndPos - yPos
-      }
-    }
-  }
-
-  p.mouseClicked = () => {
-    thr = p.random(10, 300)
-    thr2 = p.random(0, 1)
-    console.log("thr = ", thr, ", thr2 = ", thr2)
-    colorRect(0, 0, p.width, p.width)
-    divSquare(0, 0, p.width)
-  }
-
 }
 
 new p5(sketch)
